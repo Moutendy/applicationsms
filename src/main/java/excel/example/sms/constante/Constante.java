3872a78444ed1dc02c5227af8ef6759231a0ca18
c.moutendy <c.moutendy@isicod.net>
2022-08-01 14:12:08 +0100

    email

---
+++ src/main/java/excel/example/sms/constante/Constante.java
@@ -0,0 +1,13 @@
+package excel.example.sms.constante;
+
+public class Constante {
+
+	   // Replace with your email here:  
+    public static final String MY_EMAIL = "c.moutendy@isicod.net";
+
+    // Replace password!!
+    public static final String MY_PASSWORD = "MOUdjegue1994";
+
+    // And receiver!
+    public static final String FRIEND_EMAIL = "christophermoutendy@gmail.com";
+}
